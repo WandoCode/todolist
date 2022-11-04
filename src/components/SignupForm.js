f3d835b5e005d12d6d28a0a53428bdee2a604f85
wandole <mchirez@gmail.com>
2022-11-04 05:52:43 +0100

    Add logic for form validation, and to show error in the signup form

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -7,6 +7,7 @@ import signupIcon from '../assets/circle-user-solid.svg'
 
 import Button from './Button'
 import { Link } from 'react-router-dom'
+import { validateForm } from '../utils/formValidation'
 function SignupForm() {
   const dispatch = useDispatch()
   const loading = useSelector((state) => state.auth.loading)
@@ -14,13 +15,22 @@ function SignupForm() {
   const [currentFocus, setCurrentFocus] = useState()
 
   const [password, setPassword] = useState('')
+  const [confirmation, setConfirmation] = useState('')
+  const [validationErrors, setValidationErrors] = useState([])
 
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    const validationErrors = validateForm({ email, password, confirmation })
+
+    if (validationErrors.length > 0) {
+      setValidationErrors(validationErrors)
+    } else {
+      setValidationErrors([])
+    }
     dispatch(signUpMiddleware(email, password))
   }
 
-  //TODO: ajouter confirmer password
   return (
     <form className="sign-form">
       <div
@@ -34,7 +44,11 @@ function SignupForm() {
           Email
         </label>
         <input
-          className="sign-form__input"
+          className={
+            validationErrors.includes('email')
+              ? 'sign-form__input sign-form__input--error'
+              : 'sign-form__input'
+          }
           type="email"
           name="email"
           id="email"
@@ -56,13 +70,43 @@ function SignupForm() {
           Password
         </label>
         <input
-          className="sign-form__input"
+          className={
+            validationErrors.includes('password')
+              ? 'sign-form__input sign-form__input--error'
+              : 'sign-form__input'
+          }
           type="password"
           name="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           onFocus={() => setCurrentFocus('password')}
+          onBlur={() => setCurrentFocus(undefined)}
+        />
+        <img src={eyeIcon} alt="Eye" className="sign-form__icon" />
+      </div>
+      <div
+        className={
+          currentFocus === 'confirmation'
+            ? 'sign-form__row sign-form__row--focus'
+            : 'sign-form__row'
+        }
+      >
+        <label className="sign-form__label" htmlFor="confirmation">
+          Confirm password
+        </label>
+        <input
+          className={
+            validationErrors.includes('confirmation')
+              ? 'sign-form__input sign-form__input--error'
+              : 'sign-form__input'
+          }
+          type="password"
+          name="confirmation"
+          id="confirmation"
+          value={confirmation}
+          onChange={(e) => setConfirmation(e.target.value)}
+          onFocus={() => setCurrentFocus('confirmation')}
           onBlur={() => setCurrentFocus(undefined)}
         />
         <img src={eyeIcon} alt="Eye" className="sign-form__icon" />
--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -1,6 +1,38 @@
-const validateForm = (formDatas) => {
-  return true
-  // TODO: a faire
+const validateForm = ({ email, password, confirmation }) => {
+  const validationErrors = []
+
+  if (email && !emailIsValid(email)) {
+    validationErrors.push('email')
+  }
+
+  if (password && !passwordIsValid(password)) {
+    validationErrors.push('password')
+  }
+
+  if (confirmation && !confirmationIsValid(confirmation, password)) {
+    validationErrors.push('confirmation')
+  }
+
+  return validationErrors
 }
 
+const emailIsValid = (email) => {
+  const arobaseIndex = email.indexOf('@')
+  const pointIndex = email.indexOf('.')
+
+  return (
+    arobaseIndex !== -1 &&
+    pointIndex !== -1 &&
+    pointIndex > arobaseIndex &&
+    email.length >= 6
+  )
+}
+
+const passwordIsValid = (password) => {
+  return password.length >= 6
+}
+
+const confirmationIsValid = (confirmation, password) => {
+  return password.length >= 6 && confirmation === password
+}
 export { validateForm }
