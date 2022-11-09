7032490edc7ee5e4b661722a21e08aa6023029dc
Wandole <mchirez@gmail.com>
2022-11-09 14:04:42 +0100

    Add drag and drop effect and style

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,7 +5,7 @@ import {
   togglePinItem,
 } from '../redux/todos/todos.action'
 import { synchronize } from '../redux/todos/todos.middleware'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import UpdateTodoForm from './UpdateTodoForm'
 import Button from './Button'
 import { useMemo } from 'react'
@@ -13,48 +13,66 @@ import trashIcone from '../assets/trash.svg'
 import archiveIcone from '../assets/archive.svg'
 import normalIcone from '../assets/normal.svg'
 import pinIcone from '../assets/pin.svg'
+import { useRef } from 'react'
 
 function TodoItem({ todo, tempOrder, onHandleDragStart, onHandleDrop }) {
   const dispatch = useDispatch()
+  const divRef = useRef()
+  const liRef = useRef()
   const userID = useSelector((state) => state.auth.currentUser.id)
   const [editMessage, setEditMessage] = useState(false)
+  const [hovered, setHovered] = useState(0)
+  const [dragged, setDragged] = useState(false)
 
-  const buildItemClass = useMemo(() => {
+  const hoveredItemClass = useMemo(() => {
     let itemClass = 'todo-item'
-    if (todo.status === -1) itemClass += ' todo-item--archived'
-    if (todo.status === 1) itemClass += ' todo-item--pinned'
+    if (hovered === 1) itemClass += ' todo-item--hover'
+    return itemClass
+  }, [hovered])
 
+  const draggedItemClass = useMemo(() => {
+    console.log(1)
+    let itemClass = 'todo-item'
+    if (dragged) itemClass += ' todo-item--dragged'
+    return itemClass
+  }, [dragged])
+
+  const containerClassName = useMemo(() => {
+    let itemClass = 'todo-item__container'
+    if (todo.status === -1) itemClass += ' todo-item__container--archived'
+    if (todo.status === 1) itemClass += ' todo-item__container--pinned'
     return itemClass
   }, [todo.status])
 
   const handleDragStart = (e) => {
+    setDragged(true)
     onHandleDragStart(tempOrder, todo.status)
   }
+  const handleDragEnd = (e) => {
+    setDragged(false)
+  }
 
-  const handleDrop = () => {
+  const handleDrop = (e) => {
+    setHovered(0)
     onHandleDrop(tempOrder, todo.status)
   }
 
   const handleDragOver = (e) => {
     e.preventDefault() // Important for onDrop to work
+    e.stopPropagation()
   }
 
   const handleToggleArchive = (e) => {
-    e.stopPropagation()
-
     dispatch(toggleArchiveItem(tempOrder, todo.status))
     dispatch(synchronize(userID))
   }
 
   const handleTogglePin = (e) => {
-    e.stopPropagation()
-
     dispatch(togglePinItem(tempOrder, todo.status))
     dispatch(synchronize(userID))
   }
 
   const handleDelete = (e) => {
-    e.stopPropagation()
     dispatch(delTodo(tempOrder, todo.status))
     dispatch(synchronize(userID, [todo.status]))
   }
@@ -63,50 +81,66 @@ function TodoItem({ todo, tempOrder, onHandleDragStart, onHandleDrop }) {
     setEditMessage(editMessage ? false : true)
   }
 
+  const handleDragEnter = (e) => {
+    setHovered((prev) => prev + 1)
+  }
+
+  const handleDragLeave = (e) => {
+    setHovered((prev) => prev - 1)
+  }
+
   return (
     <li
       draggable={editMessage ? false : true}
-      onDragStartCapture={handleDragStart}
+      onDragStart={handleDragStart}
       onDragOver={handleDragOver}
       onDrop={handleDrop}
-      className={buildItemClass}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDragEnd={handleDragEnd}
+      className={dragged ? draggedItemClass : hoveredItemClass}
       onClick={handleClick}
+      ref={liRef}
     >
-      {editMessage ? (
-        <UpdateTodoForm
-          message={todo.message}
-          todoIndex={tempOrder}
-          status={todo.status}
-          onCloseEdit={setEditMessage}
-        />
-      ) : (
-        <>
-          <div className="todo-item__text">{todo.message}</div>
-          <div className="todo-item__btns">
-            <Button
-              image={todo.status !== 1 ? pinIcone : normalIcone}
-              text=""
-              onClickHandler={handleTogglePin}
-              tooltipText={todo.status !== 1 ? 'Pin' : 'Unpin'}
-              classesArr={['inline', 'small']}
-            />
-            <Button
-              image={todo.status !== -1 ? archiveIcone : normalIcone}
-              text=""
-              onClickHandler={handleToggleArchive}
-              tooltipText={todo.status !== 1 ? 'Archive' : 'Unarchive'}
-              classesArr={['inline', 'small']}
-            />
-            <Button
-              image={trashIcone}
-              text=""
-              onClickHandler={handleDelete}
-              tooltipText="Delete"
-              classesArr={['inline', 'small']}
-            />
-          </div>
-        </>
-      )}
+      <div className={containerClassName}>
+        {editMessage ? (
+          <UpdateTodoForm
+            message={todo.message}
+            todoIndex={tempOrder}
+            status={todo.status}
+            onCloseEdit={setEditMessage}
+          />
+        ) : (
+          <>
+            <div className="todo-item__text" ref={divRef}>
+              {todo.message}
+            </div>
+            <div className="todo-item__btns">
+              <Button
+                image={todo.status !== 1 ? pinIcone : normalIcone}
+                text=""
+                onClickHandler={handleTogglePin}
+                tooltipText={todo.status !== 1 ? 'Pin' : 'Unpin'}
+                classesArr={['inline', 'small']}
+              />
+              <Button
+                image={todo.status !== -1 ? archiveIcone : normalIcone}
+                text=""
+                onClickHandler={handleToggleArchive}
+                tooltipText={todo.status !== 1 ? 'Archive' : 'Unarchive'}
+                classesArr={['inline', 'small']}
+              />
+              <Button
+                image={trashIcone}
+                text=""
+                onClickHandler={handleDelete}
+                tooltipText="Delete"
+                classesArr={['inline', 'small']}
+              />
+            </div>
+          </>
+        )}
+      </div>
     </li>
   )
 }
