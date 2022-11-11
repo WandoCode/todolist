d41bae4ef3d32ac92e28a28d8f333a21d8f0df9b
Wandole <mchirez@gmail.com>
2022-11-11 22:51:59 +0100

    Add link to homepage in Page 404

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,5 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function Page404() {
-  return <div className="page404">Page note found. Go back to homepage</div>
+  return (
+    <div className="page404">
+      Page note found. <Link to="/">Go back to homepage</Link>
+    </div>
+  )
 }
 
 export default Page404
